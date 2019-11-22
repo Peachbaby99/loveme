ea17be3ef50c16d7bd0c3b5ecfc67f95a8f2a3bc
Administrator <111@163.com>
2019-11-22 21:14:22 +0800

    firstcommit

---
+++ Test01/src/main/java/pojo/User.java
@@ -0,0 +1,37 @@
+package pojo;
+
+import java.io.Serializable;
+
+/**
+ * @Description:
+ * @author: 向某人
+ * @date:2019/11/22 下午 09:03
+ */
+public class User implements Serializable {
+    private String name;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
